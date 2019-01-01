27f03d8df818405a59bc8a4560db742c72873ff4
Sean Mythen <mythen.sean@gmail.com>
2019-01-01 16:28:20 -0600

    Added Skills Section

---
+++ main.js
@@ -0,0 +1,11 @@
+function changeText(text)
+{
+    var display = document.getElementById('skilltitle');
+    display.innerHTML = text;
+}
+      
+function defaultText()
+{
+    var display = document.getElementById('skilltitle');
+    display.innerHTML = "Expertise";
+}
