1bc7b4acdd004975ecbdba0c9cabd923508388d4
SeanMythen <mythen.sean@gmail.com>
2019-01-07 14:42:11 -0600

    added learning section

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ function defaultText()
     var display = document.getElementById('skilltitle');
     display.innerHTML = "Expertise";
 }
+
+function changeText1(text)
+{
+    var display = document.getElementById('learningtitle');
+    display.innerHTML = text;
+}
+      
+function defaultText1()
+{
+    var display = document.getElementById('learningtitle');
+    display.innerHTML = "Learning";
+}
