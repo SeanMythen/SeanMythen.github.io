a236bf95ede629626764a3f1057335cf78279b68
SeanMythen <mythen.sean@gmail.com>
2019-01-22 23:35:45 -0600

    Added Portfolio Page

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-function changeText(text)
-{
+function changeText(text) {
     var display = document.getElementById('skilltitle');
     display.innerHTML = text;
 }
-      
-function defaultText()
-{
+
+function defaultText() {
     var display = document.getElementById('skilltitle');
     display.innerHTML = "Expertise";
 }
 
-function changeText1(text)
-{
+function changeText1(text) {
     var display = document.getElementById('learningtitle');
     display.innerHTML = text;
 }
-      
-function defaultText1()
-{
+
+function defaultText1() {
     var display = document.getElementById('learningtitle');
     display.innerHTML = "Learning";
 }
+
+let porfolio = document.getElementById('portfolio');
+let container = document.getElementById('container');
+let portfolioContainer = document.getElementById('portfolioContainer');
+
+let body = document.getElementById('body');
+
+
+
+
+window.onload = function(){
+    body.classList.remove('fade-out')
+    body.classList.remove('fade-out-portfolio')
+};
+
+
